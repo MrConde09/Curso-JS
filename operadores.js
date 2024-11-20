745ef19bd5386cf85b74fc0cb3522ecb9a0132ae
Mario Cordoba <60834426+MrConde09@users.noreply.github.com>
2024-11-20 17:23:08 -0600

    Add files via upload
    Video de Operaciones en JS

---
+++ operadores.js
@@ -0,0 +1,58 @@
+/*
+Tipos de oreadores:
+- Asignaciones
+- Matematicas y Aritmenticas
+- Incrementos y Decrementos
+- Operadores logicos de Comparación
+ - Negacion
+ - AND
+ - OR
+*/
+
+ // Asignaciones
+ var nombre, apellido, edad;
+
+apellido = "Cordoba";
+nombre = "Mario";
+edad = 22;
+
+// Matematicas y aritmeticas
+var numero1, numero2;
+numero1 = 5;
+numero2 = 6;
+
+var suma = numero1 + numero2;
+document.write(suma);
+
+var resta = numero2 - numero1;
+document.write(resta);
+
+var multi = numero1 * numero2;
+document.write(multi);
+
+var divis = numero2 / numero1;
+document.write(divis);
+
+//Incremento y Decremento
+
+
+
+//Operadores Logicos o de comparación
+
+/*
+    Operadores Logicos
+    == Igual a
+    === igual en valor y tipo
+    != No igual
+    !== No igual en valor y tipo
+    > Mayor que
+    < Menor que
+    >= Mayor o igual a que
+    <= Menor o igual a que
+*/
+
+var n1 = 5;
+var n2 = 3;
+
+var resul = n1 != n2;
+document.write(resul);
